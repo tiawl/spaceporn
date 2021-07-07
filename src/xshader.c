746e47476cf296e315b073a87fdef484cffaa66b
trapped_in_a_while_loop <pabtomas@protonmail.com>
2021-07-07 21:19:26 +0200

    updating travis.yml

--- src/xshader.c
+++ src/xshader.c
@@ -5,7 +5,8 @@ void CheckOpenGLError(const char* stmt, const char* fname, int line)
   GLenum error = glGetError();
   if (error != GL_NO_ERROR)
   {
-    fprintf(stderr, "OpenGL error %08x, at %s:%i - for %s\n", error, fname, line, stmt);
+    fprintf(stderr, "OpenGL error %08x, at %s:%i - for %s\n", error, fname,
+      line, stmt);
     abort();
   }
 }

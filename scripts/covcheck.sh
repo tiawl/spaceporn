9aafa40996ee83a6f95615511e3caa570747fb4e
trapped_in_a_while_loop <pabtomas@protonmail.com>
2022-01-03 20:12:47 +0100

    [fixing - scripts] coverage checking fixed --> memory checking needs a fix

--- scripts/covcheck.sh
+++ scripts/covcheck.sh
@@ -3,7 +3,7 @@
 declare -r FRAGMENTS=$(find shaders/fragment -type f)
 declare -r VERTEXES=$(find shaders/vertex -type f)
 
-declare -a -r ROADMAPS=($(seq 1 72))
+declare -a -r ROADMAPS=($(seq 2 73))
 
 make coverage > /dev/null 2>&1
 
@@ -12,30 +12,34 @@ declare -r XTELESKOP="./bin/cov/xteleskop"
 ${XTELESKOP} -h > /dev/null 2>&1
 ${XTELESKOP} -x -1 > /dev/null 2>&1
 ${XTELESKOP} -f 0 > /dev/null 2>&1
+${XTELESKOP} -z 0 > /dev/null 2>&1
+${XTELESKOP} -s 0 > /dev/null 2>&1
+${XTELESKOP} -s 1 -R 1 > /dev/null 2>&1
 ${XTELESKOP} -V -R -1 > /dev/null 2>&1
 ${XTELESKOP} -R 54 > /dev/null 2>&1
+${XTELESKOP} -R 54 fakefile > /dev/null 2>&1
 
 echo
 
 for ROADMAP in ${ROADMAPS[@]}; do
 
   FLAGS=""
-  if [[ ${ROADMAP} -ge 35 && ${ROADMAP} -le 47 ]]; then
+  if [[ ${ROADMAP} -ge 36 && ${ROADMAP} -le 48 ]]; then
     FLAGS="${VERTEXES}"
-  elif [[ ${ROADMAP} -ge 48 && ${ROADMAP} -le 60 ]]; then
+  elif [[ ${ROADMAP} -ge 49 && ${ROADMAP} -le 61 ]]; then
     FLAGS="${FRAGMENTS}"
   fi
 
   if [[ "x${FLAGS}" == "x" ]]; then
     printf %80s | tr ' ' '='
     echo -e "\n\nCovering roadmap ${ROADMAP} ..."
-    ${XTELESKOP} -a -m -p -x 500 -f 30 -R ${ROADMAP} &> /dev/null
+    ${XTELESKOP} -a -m -p -x 300 -f 30 -z 25 -R ${ROADMAP} &> /dev/null
     echo "Roadmap ${ROADMAP} covered" && echo
   else
     for FILE in ${FLAGS}; do
       printf %80s | tr ' ' '='
       echo -e "\n\nCovering roadmap ${ROADMAP} - ${FILE} ..."
-      ${XTELESKOP} -a -m -p -x 500 -f 30 -R ${ROADMAP} $(echo ${FILE} \
+      ${XTELESKOP} -a -m -p -x 300 -f 30 -z 25 -R ${ROADMAP} $(echo ${FILE} \
         | sed 's:^\([^/]\+/\)\{2\}::g') &> /dev/null
       echo "Roadmap ${ROADMAP} - ${FILE} covered" && echo
     done

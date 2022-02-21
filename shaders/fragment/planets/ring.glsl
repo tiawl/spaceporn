b56fcb680fabd6fb6987284828db213e9a521e74
trapped_in_a_while_loop <pabtomas@protonmail.com>
2022-02-21 21:15:37 +0100

    [update - ring] light fixed

--- shaders/fragment/planets/ring.glsl
+++ shaders/fragment/planets/ring.glsl
@@ -1,14 +1,10 @@
 # include "planets/common.glsl"
 
-const vec3[10] lightColors = vec3[](
-  vec3(0.766), vec3(0.731), vec3(0.697), vec3(0.66), vec3(0.625), vec3(0.574),
-  vec3(0.525), vec3(0.476), vec3(0.427), vec3(0.376)
-);
+const float[10] lightColors =
+  float[](0.766, 0.731, 0.697, 0.66, 0.625, 0.574, 0.525, 0.476, 0.427, 0.376);
 
-const vec3[10] darkColors = vec3[](
-  vec3(0.271), vec3(0.259), vec3(0.248), vec3(0.232), vec3(0.22), vec3(0.206),
-  vec3(0.194), vec3(0.18), vec3(0.168), vec3(0.153)
-);
+const float[10] darkColors =
+  float[](0.271, 0.259, 0.248, 0.232, 0.22, 0.206, 0.194, 0.18, 0.168, 0.153);
 
 float turbulence(float size, vec2 sizeModifier, float time_speed, vec2 coords,
   uint noise_seed, uint octaves, vec2 center)
@@ -24,7 +20,7 @@ float turbulence(float size, vec2 sizeModifier, float time_speed, vec2 coords,
   return c_noise;
 }
 
-vec3 colorSelection(vec3 colors[10], float posterized)
+float colorSelection(float colors[10], float posterized)
 {
   int pos = int(floor(posterized * 9.5));
   return colors[min(pos, 9)];
@@ -36,11 +32,11 @@ vec4 computePlanetUnder(vec2 coords, Planet planet, bool dith)
   const uint octaves = 5u;
   const vec2 sizeModifier = vec2(2., 1.);
 
-  float d_circle = length(coords);
-  float d_light = distance(coords, planet.light_origin) / sqrt(planet.radius);
-
   coords = rotate(coords, vec2(0.), planet.rotation);
   coords = spherify(coords, vec2(0.), planet.radius);
+
+  float d_circle = length(coords);
+  float d_light = distance(coords, planet.light_origin) / planet.radius;
 
   float band = ppfbm(size, sizeModifier,
     vec2(0., (coords.y + planet.center.y) * size), octaves, seed,
@@ -62,19 +58,19 @@ vec4 computePlanetUnder(vec2 coords, Planet planet, bool dith)
   fbm2 *= (dith ? 1.1 : 1.);
 
   float posterized = floor(fbm2 * 4.) / 2.;
-  vec3 col = (fbm2 < 0.625 ? colorSelection(lightColors, posterized) :
+  float col = (fbm2 < 0.625 ? colorSelection(lightColors, posterized) :
     colorSelection(darkColors, posterized - 1.));
 
-  float light_b = max(1. - d_light - (fbm1 - 0.5) * 0.35, 0.);
-  col = sqrt(col.x > 0. ? col : vec3(0.));
-  col *= min(0.8, light_b);
-  col *= (dith && (light_b < 0.8) ? 0.95 : 1.);
+  d_light = max(1. - d_light - (fbm1 - 0.5) * 0.35, 0.);
+  col = sqrt(max(col, 0.));
+  col *= min(0.8, d_light);
+  col *= (dith && (d_light < 0.8) ? 0.95 : 1.);
   col = (floor(col * PLANET_COLS)) / PLANET_COLS;
-  return vec4(col, 1.);
+  return vec4(vec3(col), 1.);
 }
 
 vec3 computeRingShape(vec2 coords, Planet planet, float rotation, float w,
-  float radius, float angle)
+  float ring_radius, float angle)
 {
   const float size = 10.;
   float width = 0.12 + w;
@@ -83,7 +79,7 @@ vec3 computeRingShape(vec2 coords, Planet planet, float rotation, float w,
 
   coords = rotate(coords, vec2(0.), rotation);
 
-  vec2 coords2 = coords * vec2(0.5 - radius, 5.7 - angle);
+  vec2 coords2 = coords * vec2(0.5 - ring_radius, 5.7 - angle);
   float center_d = distance(coords2, vec2(0.));
 
   float ring = smoothstep(0.5 - width * 2., 0.5 - width, center_d);
@@ -102,16 +98,17 @@ vec3 computeRingShape(vec2 coords, Planet planet, float rotation, float w,
 
 vec4 computeRingColor(vec2 coords, Planet planet, bool dith)
 {
-  float d_light = distance(coords, planet.light_origin) / sqrt(planet.radius);
+  coords = rotate(coords, vec2(0.), planet.rotation);
+  float d_light = distance(coords, planet.light_origin - vec2(0.5)) / sqrt(planet.radius);
   float posterized = floor((planet.ring * d_light * d_light) * 8.) / 8.;
-  vec3 col = (posterized <= 1. ? colorSelection(lightColors, posterized) :
+  float col = (posterized <= 1. ? colorSelection(lightColors, posterized) :
     colorSelection(darkColors, posterized - 1.));
 
-  float light_b = max(sqrt(1. - d_light), 0.4);
-  col *= light_b;
-  col *= (dith && (light_b < 1.) ? 0.95 : 1.);
+  d_light = max(sqrt(1. - d_light) * 1.5, 0.7);
+  col *= d_light;
+  col *= (dith && (d_light < 1.35) ? 0.95 : 1.);
   col = (floor(col * PLANET_COLS)) / PLANET_COLS;
-  return vec4(col, 1.);
+  return vec4(vec3(col), 1.);
 }
 
 vec4 ring(vec2 coords, Planet planet, bool dith)

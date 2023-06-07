78b1b9605b652f913a1a0adf7262e8155bb50cdf
tiawl <pabtomas@protonmail.com>
2023-06-07 12:30:53 +0200

    [update] c lang to zig lang in progress

--- TODO.md
+++ TODO.md
@@ -2,5 +2,4 @@
 
 spaceporn:
 - c lang to zig lang
-- bot for libs
 - vulkan tutorials -> https://vulkan-tutorial.com/Drawing_a_triangle/Setup/Validation_layers#page_Testing
--- build.zig
+++ build.zig
@@ -15,7 +15,12 @@ pub fn build(builder: *std.build.Builder) !void
     .target = target,
     .optimize = mode,
   });
-  builder.installArtifact(exe);
+
+  // Init a new install artifact step that will copy exe into destination directory
+  const install_exe = builder.addInstallArtifact(exe);
+
+  // Install step must be made after install artifact step is made
+  builder.getInstallStep().dependOn(&install_exe.step);
 
   // vulkan-zig: new step that generates vk.zig (stored in zig-cache) from the provided vulkan registry.
   const gen = vkgen.VkGenerateStep.create(builder, "libs/vulkan-zig/examples/vk.xml");
@@ -35,13 +40,21 @@ pub fn build(builder: *std.build.Builder) !void
   shaders.add("triangle_frag", "shaders/fragment/main.glsl", .{});
   exe.addModule("resources", shaders.getModule());
 
+  // Init a new run artifact step that will run exe (invisible for user)
   const run_cmd = builder.addRunArtifact(exe);
+
+  // Run artifact step must be made after install step is made
   run_cmd.step.dependOn(builder.getInstallStep());
+
+  // Allow to pass arguments from the zig build command line: zig build run -- -o foo.bin foo.asm
   if (builder.args) |args|
   {
     run_cmd.addArgs(args);
   }
 
+  // Init a new step (visible for user)
   const run_step = builder.step("run", "Run the app");
+
+  // New step must be made after run artifact step is made
   run_step.dependOn(&run_cmd.step);
 }

be88b5574d34fade896a7ad0cd6f3456c04feb19
tiawl <pabtomas@protonmail.com>
2023-06-26 18:16:52 +0200

    [fix #22] vk tuto descriptor pool and sets: removing temp files

--- test/todo.zig
+++
@@ -1,8 +0,0 @@
-const std = @import ("std");
-test "test" {
-
-  const start_time = try std.time.Instant.now();
-  //const t: f64 = @intToFloat(f64, std.time.nanoTimestamp()) / 1_000_000_000.0;
-  const t = (@floatFromInt(f32, (try std.time.Instant.now()).since(start_time)) / @floatFromInt(f32, std.time.ns_per_s));
-  std.debug.print ("{d:.9}\n", .{t});
-}

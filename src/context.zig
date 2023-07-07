a97802013e06de7bfa25d20ad9e3b3312a0135af
tiawl <pabtomas@protonmail.com>
2023-07-07 13:31:36 +0200

    [update #97] import cimgui: in progress

--- src/context.zig
+++ src/context.zig
@@ -3,13 +3,8 @@ const std = @import ("std");
 const context_vk   = @import ("vk/context.zig").context_vk;
 const context_glfw = @import ("glfw/context.zig").context_glfw;
 
-const build   = @import ("build_options");
-const LOG_DIR = build.LOG_DIR;
-
 const utils    = @import ("utils.zig");
 const log_app  = utils.log_app;
-const log_file = utils.log_file;
-const profile  = utils.profile;
 const severity = utils.severity;
 
 const opts = @import ("options.zig").options;
@@ -21,46 +16,8 @@ pub const context = struct
 
   const Self = @This ();
 
-  fn init_logfile () !void
-  {
-    if (build.LOG_LEVEL > @intFromEnum (profile.TURBO) and build.LOG_DIR.len > 0)
-    {
-      var dir = std.fs.cwd ().openDir (LOG_DIR, .{}) catch |err|
-      {
-        if (err == std.fs.File.OpenError.FileNotFound)
-        {
-          try log_app ("{s} does not exist, impossible to log execution.", severity.ERROR, .{ LOG_DIR });
-        }
-        return err;
-      };
-
-      defer dir.close ();
-
-      const file = std.fs.cwd ().openFile (log_file, .{}) catch |open_err| blk:
-      {
-        if (open_err != std.fs.File.OpenError.FileNotFound)
-        {
-          try log_app ("failed to open log file", severity.ERROR, .{});
-          return open_err;
-        } else {
-          const cfile = std.fs.cwd ().createFile (log_file, .{}) catch |create_err|
-          {
-            try log_app ("failed to create log file", severity.ERROR, .{});
-            return create_err;
-          };
-          break :blk cfile;
-        }
-      };
-
-      defer file.close ();
-    }
-    try log_app ("log file init OK", severity.DEBUG, .{});
-  }
-
   pub fn init (allocator: std.mem.Allocator, options: opts) !Self
   {
-    try init_logfile ();
-
     var self = Self {};
 
     self.glfw = try context_glfw.init (options);

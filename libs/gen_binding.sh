e13a3635818eb538f91c83cd3ec3a673b368ccd8
tiawl <pabtomas@protonmail.com>
2023-08-04 13:17:34 +0200

    [update #117] dear_bindings instead of cimgui

--- libs/gen_binding.sh
+++ libs/gen_binding.sh
@@ -7,9 +7,11 @@ main ()
   old_ifs="${IFS}"
   ld="$(CDPATH=; cd "$(dirname "${0}")"; pwd -P)"
   readonly old_ifs ld
+  git checkout --force backend_conversion &> /dev/null
   python3 "${ld}/dear_bindings/dear_bindings.py" --output "${ld}/cimgui" "${ld}/imgui/imgui.h"
   python3 "${ld}/dear_bindings/dear_bindings.py" --backend --output "${ld}/cimgui_impl_vulkan" "${ld}/imgui/backends/imgui_impl_vulkan.h"
   python3 "${ld}/dear_bindings/dear_bindings.py" --backend --output "${ld}/cimgui_impl_glfw"   "${ld}/imgui/backends/imgui_impl_glfw.h"
+  git checkout --force main &> /dev/null
   rm -f "${ld}"/*.json
 }
 
